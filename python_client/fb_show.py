441adad297f1c1b6ad6be9ca93add24fb25b906b
ViktorFagerlind <viktor_fagerlind@hotmail.com>
2024-10-02 07:56:51 +0200

    Starting FB client

---
+++ python_client/fb_show.py
@@ -0,0 +1,57 @@
+import logging
+from training_server_proxy import TrainingServerProxy
+import grpc
+from ..garmin_server.training_fs_db import TrainingFsDb
+
+import sys
+import argparse
+from plots import plot_fitness_trend, plot_garmin_trend, plot_activities
+
+# Configure debug logging
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+
+def connect():
+    tdb = TrainingFsDb()
+    tdb.connect("trainingtrends-2d3fb-firebase-adminsdk-w82rb-1949489f7a.json")
+    return tdb
+
+
+if __name__ == '__main__':
+    if not len(sys.argv) > 1:
+        print('Without arguments, nothing will be shown :)')
+        exit()
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-ft', '--fitness_trend', action='store_true')
+    parser.add_argument('-gt', '--garmin_trend', action='store_true')
+    parser.add_argument('-hrt', '--heartrate_trend', action='store_true')
+    args = parser.parse_args()
+
+    tdb = connect()
+
+    fig = None
+
+    if args.fitness_trend:
+        fitness_trend_df = tdb.get_fitness_trend('FITNESS_TREND')
+        raw_trend_data_df = tdb.get_raw_trend_data()
+        print(fitness_trend_df.to_string())
+        print(raw_trend_data_df.to_string())
+        fig = plot_fitness_trend(fitness_trend_df, raw_trend_data_df, '2022-09-10')
+
+    if args.garmin_trend:
+        fitness_trend_df = tdb.get_fitness_trend('GARMIN_TREND')
+        raw_trend_data_df = tdb.get_raw_trend_data()
+        print(fitness_trend_df.to_string())
+        print(raw_trend_data_df.to_string())
+        fig = plot_garmin_trend(fitness_trend_df, raw_trend_data_df, '2022-09-10')
+
+    if args.heartrate_trend:
+        activities_df = tdb.get_df_activities()
+        raw_trend_data_df = tdb.get_raw_trend_data()
+        print(activities_df.to_string())
+        fig = plot_activities(activities_df, raw_trend_data_df, '2022-09-10')
+
+    if fig is not None:
+        fig.show()
